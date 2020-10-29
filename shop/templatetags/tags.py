77283cce75cf3e7b5e60fa30a6bce5cd1a386abd
maheshschand <r.mahesh2307@gmail.com>
2020-10-29 19:28:10 +0530

    Add other applications

---
+++ shop/templatetags/tags.py
@@ -0,0 +1,7 @@
+from django import template
+from django.utils.safestring import mark_safe
+import markdown
+register = template.Library()
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
